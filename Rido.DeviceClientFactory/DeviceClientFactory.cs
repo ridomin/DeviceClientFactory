8ef19ec7f847f0e050087e05401d478e1cb2f3ce
ridomin <ridomin@live.com>
2020-02-26 11:03:14 -0800

    rename test project

--- Rido.DeviceClientFactory/DeviceClientFactory.cs
+++ Rido.DeviceClientFactory/DeviceClientFactory.cs
@@ -5,7 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
-[assembly: InternalsVisibleToAttribute("RealTests")]
+[assembly: InternalsVisibleToAttribute("Tests")]
 
 namespace Rido
 {
