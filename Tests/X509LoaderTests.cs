a6911a29f70e23517e24cfd9588021374a05d9a6
Ricardo Minguez (Rido) <rido-min@users.noreply.github.com>
2020-02-29 22:19:23 -0800

    Config ci (#3)
    * expand wildcar
    
    * run tests
    
    * test in rel
    
    * comment test with cert
    
    * fix cert store tests
    
    * include ssnupkg
    
    * push
    
    * add nuget.config
    
    * dont clear
    
    * add nuget.org
    
    * restore from nuget
    
    * restore from nuget
    
    * rename nuget.config
    
    * dont publish
    
    Co-authored-by: Rido <ridomin@live.com>

--- Tests/X509LoaderTests.cs
+++ Tests/X509LoaderTests.cs
@@ -1,9 +1,9 @@
-﻿using Rido;
+using Rido;
 using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
-
+using System.Security.Cryptography.X509Certificates;
 namespace Tests
 {
     public class X509LoaderTests
@@ -25,10 +25,19 @@ namespace Tests
         }
 
         [Fact]
-        public void ParseCertConfigFromThumbprint()
+        public void CertFromStore()
         {
-            string certConfigWithPassswd = "261aa9ae4024ea9ad23297c7c4c3c5579692445e";
-            var cert = X509Loader.GetCertFromConnectionString(certConfigWithPassswd);
+            string certConfigWithPassswd = "mycertnopasswd.pfx";
+            var cert1 = X509Loader.GetCertFromConnectionString(certConfigWithPassswd);
+            X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
+            store.Add(cert1);
+            System.Console.WriteLine(cert1.Thumbprint);
+            store.Close();
+
+
+            string certThumbprint = "29AD021F61B31D505CF64445B6269606E79063B4";
+            var cert = X509Loader.GetCertFromConnectionString(certThumbprint);
             Assert.NotNull(cert);
         }
     }
