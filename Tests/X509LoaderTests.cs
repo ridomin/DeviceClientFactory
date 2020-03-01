6cd4e093cd4879c555ecd9835ac9a5af7278149f
ridomin <ridomin@live.com>
2020-03-01 05:33:02 +0000

    fix cert store tests

--- Tests/X509LoaderTests.cs
+++ Tests/X509LoaderTests.cs
@@ -3,7 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using Xunit;
-
+using System.Security.Cryptography.X509Certificates;
 namespace Tests
 {
     public class X509LoaderTests
@@ -24,11 +24,20 @@ namespace Tests
             Assert.NotNull(cert);
         }
 
-        //Fact]
-        public void ParseCertConfigFromThumbprint()
+        [Fact]
+        public void CertFromStore()
         {
-            string certConfigWithPassswd = "261aa9ae4024ea9ad23297c7c4c3c5579692445e";
-            var cert = X509Loader.GetCertFromConnectionString(certConfigWithPassswd);
+            string certConfigWithPassswd = "mycertnopasswd.pfx";
+            var cert1 = X509Loader.GetCertFromConnectionString(certConfigWithPassswd);
+            X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
+            store.Open(OpenFlags.ReadWrite);
+            store.Add(cert1);
+            System.Console.WriteLine(cert1.Thumbprint);
+            store.Close();
+
+
+            string certThumbprint = "29AD021F61B31D505CF64445B6269606E79063B4";
+            var cert = X509Loader.GetCertFromConnectionString(certThumbprint);
             Assert.NotNull(cert);
         }
     }
