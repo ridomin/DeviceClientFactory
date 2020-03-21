a41e5be15cd979a1354ad95141f9344a779dbbf0
ridomin <ridomin@live.com>
2020-03-21 00:18:22 -0700

    ref. Static Method, Obsolete Ctor, Add internal logging

--- TestDevice/Program.cs
+++ TestDevice/Program.cs
@@ -9,14 +9,14 @@ namespace TestDevice
     {
         const string directSas = "HostName=e2e-test-hub.azure-devices.net;DeviceId=test-sas-01;SharedAccessKey=pRNwmc8UU0fH6vnTZ50PmfqGffii5fWWLNfdQOaBsu8=";
         const string directSSCert = "HostName=e2e-test-hub.azure-devices.net;DeviceId=test-ss-cert-01;X509=mycert.pfx|1234";
-        const string directCACert = "HostName=e2e-test-hub.azure-devices.net;DeviceId=test-cert-ca-01;X509=01632B87BED25142DD2F1337AA50CCF1B0F79831";
+        //const string directCACert = "HostName=e2e-test-hub.azure-devices.net;DeviceId=test-cert-ca-01;X509=01632B87BED25142DD2F1337AA50CCF1B0F79831";
         //const string directInvalidCACert = "HostName=e2e-test-hub.azure-devices.net;DeviceId=test-cert-ca-01;X509=INVALID01632B87BED25142DD2F1337AA50CCF1B0F79831";
-        const string unauthorizedCert = "HostName=e2e-test-hub.azure-devices.net;DeviceId=test-cert-ca-01;X509=2afebca112d69de4b22f349a46f70991555ed31e";
+        //const string unauthorizedCert = "HostName=e2e-test-hub.azure-devices.net;DeviceId=test-cert-ca-01;X509=2afebca112d69de4b22f349a46f70991555ed31e";
 
         const string DPSCert = "ScopeID=0ne000C6BF3;DcmId=urn:rigado:S1_Sensor:3;X509=08B7EAEC77B9061F05385D54EE4CC00EA754DB36";
 
-        static ILogger logger = LoggerFactory.Create(builder => { builder.AddConsole(); }).CreateLogger("Cat1");
-        static async Task Main(string[] args)
+        static readonly ILogger logger = LoggerFactory.Create(builder => { builder.AddConsole(); }).CreateLogger("Cat1");
+        static async Task Main()
         {
             //await ConnectDevice(unauthorizedCert, nameof(unauthorizedCert)).ConfigureAwait(false);
             //await ConnectDevice(directCACert, nameof(directCACert)).ConfigureAwait(false);
@@ -28,8 +28,7 @@ namespace TestDevice
         private static async Task ConnectDevice(string cs, string name)
         {
             Console.WriteLine("Connecting: " + name);
-            var dcf = new DeviceClientFactory(cs, logger);
-            var dc = await dcf.CreateDeviceClientAsync().ConfigureAwait(false);
+            var dc = await DeviceClientFactory.CreateDeviceClientAsync(cs, logger).ConfigureAwait(false);
 
             dc.SetConnectionStatusChangesHandler(
                 (Microsoft.Azure.Devices.Client.ConnectionStatus status,
--- Tests/X509LoaderTests.cs
+++ Tests/X509LoaderTests.cs
@@ -4,15 +4,19 @@ using System.Collections.Generic;
 using System.Text;
 using Xunit;
 using System.Security.Cryptography.X509Certificates;
+using Microsoft.Extensions.Logging;
+
 namespace Tests
 {
     public class X509LoaderTests
     {
-       [Fact]
+
+       static readonly ILogger logger = LoggerFactory.Create(builder => { builder.AddConsole(); }).CreateLogger("Cat1");
+        [Fact]
        public void ParseCertConfigFromFileWithPasswd()
         {
             string certConfigWithPassswd = "mycert.pfx|1234";
-            var cert = X509Loader.GetCertFromConnectionString(certConfigWithPassswd);
+            var cert = X509Loader.GetCertFromConnectionString(certConfigWithPassswd, logger);
             Assert.NotNull(cert);
         }
 
@@ -20,7 +24,7 @@ namespace Tests
         public void ParseCertConfigFromFileNoPasswd()
         {
             string certConfigWithPassswd = "mycertnopasswd.pfx";
-            var cert = X509Loader.GetCertFromConnectionString(certConfigWithPassswd);
+            var cert = X509Loader.GetCertFromConnectionString(certConfigWithPassswd, logger);
             Assert.NotNull(cert);
         }
 
@@ -28,7 +32,7 @@ namespace Tests
         public void CertFromStore()
         {
             string certConfigWithPassswd = "mycertnopasswd.pfx";
-            var cert1 = X509Loader.GetCertFromConnectionString(certConfigWithPassswd);
+            var cert1 = X509Loader.GetCertFromConnectionString(certConfigWithPassswd, logger);
             X509Store store = new X509Store(StoreName.My, StoreLocation.CurrentUser);
             store.Open(OpenFlags.ReadWrite);
             store.Add(cert1);
@@ -37,7 +41,7 @@ namespace Tests
 
 
             string certThumbprint = "29AD021F61B31D505CF64445B6269606E79063B4";
-            var cert = X509Loader.GetCertFromConnectionString(certThumbprint);
+            var cert = X509Loader.GetCertFromConnectionString(certThumbprint, logger);
             Assert.NotNull(cert);
         }
     }
