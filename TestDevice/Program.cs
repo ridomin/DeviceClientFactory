4009ae90d99092f10a64152f4aade670148cd308
rido-min <rido-min@users.noreply.github.com>
2021-05-04 22:22:38 -0700

    update to 1.37

---
+++ TestDevice/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Azure.Devices.Client;
+using Rido;
+using System;
+using System.Threading.Tasks;
+
+namespace TestDevice
+{
+    class Program
+    {
+        static string CS = Environment.GetEnvironmentVariable("CS");
+        static async Task Main(string[] args)
+        {
+            var dc = await DeviceClientFactory.CreateDeviceClientAsync(CS, "dtmi:com:example:Thermostat;1");
+            await dc.OpenAsync();
+            Console.WriteLine($"Device {DeviceClientFactory.Instance.DeviceId} Connected to {DeviceClientFactory.Instance.HostName}");
+            await Task.Delay(500);
+            await dc.CloseAsync();
+        }
+    }
+}
